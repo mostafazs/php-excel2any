42b2ca3c3384e5aa7443f8d9e2476bdfc38e53fa
Mostafa <mostafas.dev@gmail.com>
2022-09-17 23:53:27 +0430

    Some exception added

--- src/Saver/SaveCSV.php
+++ src/Saver/SaveCSV.php
@@ -23,8 +23,14 @@ Class SaveCSV implements SaverInterface{
             $output  = "\xEF\xBB\xBF";
             $output .= $data;
             $filename = $filename.".csv";//TODO improvement
-            $fh = fopen($filename,"w");//TODO improvement catch bool(resource) exception
+            $fh = fopen($filename,"w");
+            if($fh!=true){
+                throw new RuntimeException("File open exception");
+            }
             $result = fwrite($fh,$output);
+            if($result==false){
+                throw new RuntimeException("File write exception");
+            }
             if($result == FALSE){
                 throw new RuntimeException("Error write file");
             }else{
